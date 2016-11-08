f67a5c3e99fc8cf5de46971ad031f7a9408e1108
Mathias Lebout <mathias.lebout@gmail.com>
2016-11-08 17:15:54 +0100

    Added CORS header

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ var getStore = function(req, res, next) {
         res.send(products);
         next();
     };
-    productModel.find({}).select({description:1, price:1, sizes:1}).exec(onFind);
+    // productModel.find({}).select({description:1, price:1, sizes:1, colors: 1, category: 1, href: 1}).exec(onFind);
+    productModel.find({}).exec(onFind);
 }
 
 var getBySize = function(req, res, next) {
@@ -37,20 +38,34 @@ db.once('open', function() {
     var productSchema = mongoose.Schema({
         id: String,
         description: String,
+        category: String,
+        href: String,
+        imgHref: String,
         price: Number,
-        sizes: Array
+        sizes: Array,
+        colors: Array,
+        shop: String
     });    
 
     productModel = mongoose.model('Product', productSchema);
 
     var server = restify.createServer();
+
+    server.use(
+        function crossOrigin(req,res,next){
+            res.header("Access-Control-Allow-Origin", "*");
+            res.header("Access-Control-Allow-Headers", "X-Requested-With");
+            return next();
+        }
+    );
+
     server.get('/hello/:name', respond);
     server.head('/hello/:name', respond);
 
     server.get('/store/', getStore);
     server.get('/store/size/:size', getBySize);
 
-    server.listen(8080, function() {
+    server.listen(8082, function() {
         console.log('%s listening at %s', server.name, server.url);
     });
 });
